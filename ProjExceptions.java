554b366d96570381ae3f37395a8d66a350690481
agreysrivastava <35161379+agreysrivastava@users.noreply.github.com>
2020-06-24 18:45:42 +0530

    Update ProjExceptions.java

--- ProjExceptions.java
+++ ProjExceptions.java
@@ -1,155 +1,129 @@
-package projexceptions;
 import java.io.*;
-import java.util.*;
-/*
- * @author user
- */
+
 abstract class A{
-    int x=5,y=2;
-    final int z=15;
-    A(){
-        System.out.println("Constructor of base class");
+    int i=10;
+    String str = "Hello";
+    public void print(Object o){
+        System.out.println(o);
     }
-    public void change(int X,int Y){
-        x=X;
-        y=Y;
-    }
-     abstract void calculate();
+    public abstract void calculate();
+    public abstract void compare(A a);
 }
 interface B{
-    int i=10;
-    void chnge(int a);
-    void calc();
+    int x =10;
+    public void calc();
+    public void comp();
 }
-public class ProjExceptions  extends A implements B{
-    
-    
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        /*ArrayList arr= new ArrayList();
-        for(int i=0;i<5;i++){
-            arr.add(br.readLine());
-        }
-        Iterator it= arr.iterator();
-        while(it.hasNext()){
-            Object o=it.next();
-            System.out.println(o+" "+o.getClass());
-            
-        }*/
-        A a =new A() {
-            @Override
-            void calculate() {
-                int c=x+y+z;
-                System.out.println(c);
-            }
-        };
-        A o=new A(){
-            @Override
-            void calculate() {
-                int p=x*y*z;
-                System.out.println(p);
-            }
-            
-        };
-        B b=new B() {
-            @Override
-            public void chnge(int a) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
+public class ProjAbstract extends A implements B{
 
+    public static void main(String[] args) throws IOException {
+        A a = new A(){
+            @Override
+            public void calculate() {
+                int prod=1;
+                int x;
+               for( x = i;x>1;x--){
+                   prod=prod*x;
+               }
+               i++;
+               print(prod);
+            }
+            @Override
+            public void compare(A a) {
+                if(this.i==a.i){
+                    print(true+" i value is equal");
+                }
+                else
+                    print(false+" value of i changed");
+            }    
+        };
+        
+        A a1 = new A(){
+             @Override
+            public void calculate() {
+                float sum = i*(i+1)/2;
+                str=str+" ";
+                print(sum);
+            }
+            @Override
+            public void compare(A a) {
+                if(this.str.equals(a.str))
+                    print(true+" str matched");
+                else
+                    print(false+" str changed");
+            }  
+        };
+        a.compare(a1);
+        a1.compare(a);
+        a.calculate();
+        a1.calculate();
+        a.compare(a1);
+        a1.compare(a);
+        B b = new B() {
             @Override
             public void calc() {
                 int prod=1;
-                for(int x=1;x<=i;x++){
-                    prod*=x;
-                }
-                System.out.println(prod);
+                int i;
+               for( i = x;i>1;i--){
+                   prod=prod*i;
+               }
+               i++;
+               a.print(prod);
+            }
+            @Override
+            public void comp() {
+                if(x==10)
+                    a.print(true);
             }
         };
-        a.change(10, 5);
-        a.calculate();
-        o.change(10, 5);
-        o.calculate();
-        ProjExceptions p=new ProjExceptions();
-        p.change(100, 63);
-        p.calculate();
         b.calc();
-        p.calc();
+        b.comp();
     }
 
     @Override
-    void calculate() {
-        int c=x-y;
-        
-        System.out.println(c);
+    public void calculate() {
+        int prod=1;
+                int x;
+               for( x = i;x>1;x--){
+                   prod=prod*x;
+               }
+               i++;
+               print(prod);
+               float sum = i*(i+1)/2;
+                str=str+" ";
+                print(sum);
     }
 
     @Override
-    public void chnge(int a) {
-        
-    }
-
-    @Override
-    public void calc(){
-        for(int x=1;x<=10;x++){
-            System.out.println(i*x);
-        }
-    }
-
-    
-    
-}
-
-
- /*
-            
-public static int calc(ArrayList<Integer> dividend,ArrayList<Integer> rem) {
-        int x=1,j=0;
-        while(true){
-            System.out.println("Entered");
-            for( j=0;j<5;j++){
-                if((dividend.get(j)<0)||(rem.get(j)<0)){
-                    assert x==1:"Can't execute";
-                throw new ArithmeticException("No negeative numbers allowed");
-                
+    public void compare(A a) {
+        if(this.i==a.i){
+                    print(true+" i value is equal");
                 }
-                if(dividend.get(j)%x!=rem.get(j)){
-                    x++;
-                    break;
-                }
-            }
-            System.out.println(x+" "+j);
-            if(j==5)
-                return x;
-        }
+                else
+                    print(false+" value of i changed");
+        if(this.str.equals(a.str))
+                    print(true+" str matched");
+                else
+                    print(false+" str changed");
+    }
+
+    @Override
+    public void calc() {
+        int prod=1;
+                int i;
+               for( i = x;i>1;i--){
+                   prod=prod*i;
+               }
+               i++;
+               print(prod);
+    }
+
+    @Override
+    public void comp() {
+        if(x==10)
+                    print(true);
+        else
+            print(false);
+    }
+
 }
-            ===============================Inside main==================
-        try{
-            Scanner sc=new Scanner(System.in);
-            petrol obj=new petrol();
-            if(obj.calc())
-                System.out.println("Alliteration exists");
-            else
-                System.out.println("Alliteration does not exists");
-            ArrayList<Integer> myArr=new ArrayList<Integer>();
-            ArrayList<Integer> div=new ArrayList<Integer>();
-            for(int i=0;i<5;i++){
-                myArr.add(sc.nextInt());
-            }
-            for(int i=0;i<5;i++){
-                div.add(sc.nextInt());
-            }
-            int r=calc(myArr,div);
-            System.out.println(r);
-        }
-        catch(ArithmeticException e){
-            System.out.println(e.getMessage());
-        }
-        catch(Exception e){
-            System.out.println(e.getClass()+" "+e.getMessage());
-        }
-        finally{
-            System.out.println("Code executed successfully");
-        }    
-*/
